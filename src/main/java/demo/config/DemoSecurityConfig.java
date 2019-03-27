c86a70c07147fa61563b335db4e11a7dba6a297d
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-03-27 18:42:01 +0100

    change data from static file to JDBC structure

--- src/main/java/demo/config/DemoSecurityConfig.java
+++ src/main/java/demo/config/DemoSecurityConfig.java
@@ -1,5 +1,6 @@
 package demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,19 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    //add a reference to our security data source
+    @Autowired
+    private DataSource securityDataSource;
+
     @Override //configure users (in memory, database, ...)
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        //add users for in memory authentication
-        User.UserBuilder user = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(user.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(user.username("mary").password("test123").roles("EMPLOYEE","MANAGER"))
-                .withUser(user.username("peter").password("test123").roles("EMPLOYEE","ADMIN"));
+        //źródło danych wskaże na tabele danych, które stworzyliśmy
+       auth.jdbcAuthentication().dataSource(securityDataSource);
     }
 
     @Override //Configure security of web paths in application, login, logout,...
