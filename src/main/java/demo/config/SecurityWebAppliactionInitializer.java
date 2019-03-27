2fceea9b7af770c3054addf6aebf4294f1bc5dba
PiotrTomaszewski <piotr.tomaszewski@codecool.com>
2019-03-27 10:09:56 +0100

    spring security initializer with default login page

---
+++ src/main/java/demo/config/SecurityWebAppliactionInitializer.java
@@ -0,0 +1,8 @@
+package demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebAppliactionInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
+
